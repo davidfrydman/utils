c53b60920f958274d423e236171b86cbbed053af
davidfrydman <34936279+davidfrydman@users.noreply.github.com>
2021-12-18 22:40:44 +0200

    Update sugh.sh
    Line 10-11 : create a sub-directory and store the repository in that directory
    Lines 19-28: store all the repository - stop when the page doesn't contains any "git_url" string (tested line 23)

--- sugh.sh
+++ sugh.sh
@@ -7,12 +7,24 @@ else
     name=$1
 fi
 
+mkdir -p "$name"
+cd "$name"
 
 cntx="users"
+counter="1"
 
-echo $name
-echo $cntx
+echo name: $name
+echo cntx: $cntx
 
-curl -u $name "https://api.github.com/$cntx/$name/repos?page=1&per_page=100" | grep -e 'git_url*' | cut -d \" -f 4 | xargs -L1 git clone
+while true
+do
+	echo page: $counter
+	page=$(curl "https://api.github.com/$cntx/$name/repos?page=$counter&per_page=100")
+	if [ ! $(echo "$page" |grep -e 'git_url') ]; then
+		break;
+	fi
+	echo -e "$page" | grep -e 'git_url*' | cut -d \" -f 4 | xargs -L1 git clone
+	counter=$(expr $counter + 1)
+done
 
 exit 0
